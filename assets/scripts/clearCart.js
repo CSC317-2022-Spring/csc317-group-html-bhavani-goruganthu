ec43a20d2bdf177d41cfbff77ca139004cae58ce
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-04-13 16:11:59 -0700

    removed unused files

--- assets/scripts/clearCart.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-   // localStorage.setItem("numOfItems", 1)
-   let clearCartButton = document.getElementById("clear_cart");
-   clearCartButton.addEventListener('click', clearCart)
-   function clearCart() {
-      localStorage.setItem("numOfItems", 0)
-      // localStorage.clear()
-   }
-}
