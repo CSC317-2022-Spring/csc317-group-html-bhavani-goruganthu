d3a3587cc1aa11935f82344c1f24d78c7331d7a5
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-04-13 16:13:41 -0700

    fetched, processed & added cart products dynamically

---
+++ assets/scripts/cart.js
@@ -0,0 +1,87 @@
+// get current cart products from localstorage
+const currentCartProducts =
+  JSON.parse(localStorage.getItem('cartProducts')) || [];
+
+if (currentCartProducts.length === 0) {
+  document.getElementById(
+    'cart-count'
+  ).innerText = `(Count: ${currentCartProducts.length})`;
+} else {
+  let cartLength = 0;
+  Object.values(currentCartProducts).filter((prod) => {
+    cartLength += parseInt(prod.quantity);
+  });
+  document.getElementById('cart-count').innerText = `(Count: ${cartLength})`;
+}
+
+if (currentCartProducts.length > 0) {
+  // fetch products from json file by id & category
+  console.log(currentCartProducts);
+  fetch('../assets/scripts/products.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let fetchedCartProducts = [];
+      Object.values(currentCartProducts).filter((prod) => {
+        let filteredByCategory = Object.entries(data).filter(
+          (key) => key.includes(prod.type) // get the products filtered by category
+        );
+        // the products that belong to the category are in filteredCategory[0][1]
+        const filteredProductsByName = filteredByCategory[0][1].filter(
+          (e) => e.id === parseInt(prod.id)
+        );
+        fetchedCartProducts.push({
+          type: prod.type,
+          products: filteredProductsByName[0],
+          quantity: prod.quantity,
+        });
+      });
+      console.log(fetchedCartProducts);
+      return fetchedCartProducts;
+    })
+    .then((fetchedCartProducts) => {
+      // dynamically fill the cart page
+      fetchedCartProducts.forEach((value) => {
+        document.getElementById('cart-tbody').innerHTML += `<tr>
+        <td>
+          <a href="./productPage.html?type=${value.type}&id=${
+          value.products.id
+        }">
+            <img class="cart-product-image" width="150" height="150"
+              src=".${value.products.imageUrl}">
+          </a>
+        </td>
+        <td>
+          <h5>${value.products.name}</h5>
+        </td>
+        <td>
+          <h5>$${value.products.price}</h5>
+        </td>
+        <td>
+          <div  class="quantity-delete-div">
+          <select id="cart-quantity-dropdown" class="quantity-dropdown">
+            <option value="1"${value.quantity == 1 ? 'selected' : ''}>1</option>
+            <option value="2"${value.quantity == 2 ? 'selected' : ''}>2</option>
+            <option value="3"${value.quantity == 3 ? 'selected' : ''}>3</option>
+            <option value="4"${value.quantity == 4 ? 'selected' : ''}>4</option>
+            <option value="5"${value.quantity == 5 ? 'selected' : ''}>5</option>
+            <option value="6"${value.quantity == 6 ? 'selected' : ''}>6</option>
+            <option value="7"${value.quantity == 7 ? 'selected' : ''}>7</option>
+            <option value="8"${value.quantity == 8 ? 'selected' : ''}>8</option>
+            <option value="9"${value.quantity == 9 ? 'selected' : ''}>9</option>
+          </select>
+          <span class="material-icons quantity-delete-icon">
+            delete
+          </span>
+        </div>
+        </td>
+        <td>
+          <h5>$${parseFloat(value.products.price * value.quantity).toFixed(
+            2
+          )}</h5>
+        </td>
+                </tr>`;
+      });
+    });
+}
