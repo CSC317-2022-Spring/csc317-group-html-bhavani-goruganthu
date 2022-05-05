d18dc1a87c2d5bb9d51128a49fe31bbc3884b486
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-04 19:31:21 -0700

    home page now fetches data from db

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,69 +1,55 @@
-// Fetch products data from json file and display the products on the home page
-// Inserting to the DOM along with the classnames.
-// Any change in the json file - updates the home page automatically
-fetch('../assets/data/products.json')
+// fetch the list of categories from the db
+let apiRoute = '';
+fetch('http://localhost:4000/api/products/getAllCategories')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    data.Succulents.forEach((succulent) => {
-      document.getElementById(
-        'succulents-div'
-      ).innerHTML += `<div class="product-container">
-          <a href="./src/productPage.html?type=Succulents&id=${succulent.id}">
-            <img class="product-img" width="300" height="300" src="${succulent.imageUrl}">
-          </a>
-          <div class="product-description-container">
-            <p class="product-name">${succulent.name}</p>
-            <p class="product-price"> $${succulent.price}</p>
-          </div>
-        </div>`;
+    const categories = [];
+    Object.values(data).forEach((data) =>
+      categories.push(data.productCategory)
+    );
+    return categories;
+  })
+  .then((categories) => {
+    categories.forEach((category) => {
+      // set the apiRoute
+      apiRoute = `http://localhost:4000/api/search/searchByCategory?category=${category}`;
+      // Fetch products from the backend and display the products on the home page
+      fetch(apiRoute)
+        .then((res) => {
+          return res.json();
+        })
+        .then((fetchedProducts) => {
+          displayProductCategory(category);
+          displayFetchedProducts(fetchedProducts, category);
+        })
+        .catch((err) => console.log(err));
     });
-    data.IndoorPlants.forEach((indoorPlant) => {
-      document.getElementById(
-        'indoor-plants-div'
-      ).innerHTML += `<div class="product-container">
-            <a href="./src/productPage.html?type=IndoorPlants&id=${indoorPlant.id}">
-              <img class="product-img" width="300" height="300" src="${indoorPlant.imageUrl}">
-            </a>
-            <div class="product-description-container">
-              <p class="product-name">${indoorPlant.name}</p>
-              <p class="product-price"> $${indoorPlant.price}</p>
-            </div>
-          </div>`;
-    });
-    data.Herbs.forEach((herb) => {
-      document.getElementById(
-        'herbs-div'
-      ).innerHTML += `<div class="product-container">
-              <a href="./src/productPage.html?type=Herbs&id=${herb.id}">
-                <img class="product-img" width="300" height="300" src="${herb.imageUrl}">
-              </a>
-              <div class="product-description-container">
-                <p class="product-name">${herb.name}</p>
-                <p class="product-price"> $${herb.price}</p>
-              </div>
-            </div>`;
-    });
-    data.Shrubs.forEach((shrub) => {
-      document.getElementById(
-        'shrubs-div'
-      ).innerHTML += `<div class="product-container">
-                <a href="./src/productPage.html?type=Shrubs&id=${shrub.id}">
-                  <img class="product-img" width="300" height="300" src="${shrub.imageUrl}">
-                </a>
-                <div class="product-description-container">
-                  <p class="product-name">${shrub.name}</p>
-                  <p class="product-price"> $${shrub.price}</p>
-                </div>
-              </div>`;
-    });
+  })
+  .catch((err) => console.error(err));
+
+// add Category heading to the DOM
+function displayProductCategory(category) {
+  document.getElementById('home').innerHTML += `      <div id="row">
+  <h2 class="plant-category">${category}</h2>
+  <div id="${category}-div" class="category-container">
+  </div>
+  </div>`;
+}
+
+// Inserting products to the DOM along with the classnames
+function displayFetchedProducts(fetchedProducts, category) {
+  fetchedProducts.forEach((product) => {
+    document.getElementById(`${category}-div`).innerHTML += ` 
+    <div class="product-container">
+    <a href="./src/productPage.html?type=${product.productCategory}&id=${product.productID}">
+      <img class="product-img" width="300" height="300" src="${product.productImageUrl}">
+    </a>
+    <div class="product-description-container">
+      <p class="product-name">${product.productName}</p>
+      <p class="product-price"> $${product.productPrice}</p>
+    </div>
+  </div>`;
   });
-
-document.querySelectorAll('.product-container').forEach((productContainer) => {
-  productContainer.addEventListener('click', productContainerClick);
-});
-
-function productContainerClick(e) {
-  console.log(e.target);
 }
