055c622937b7d52e4332d35a7e2feabf55c0f2ff
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-09 15:40:46 -0700

    added fetchUserDetails & updateUserDetails routes

--- backend/endpoints/users.js
+++ backend/endpoints/users.js
@@ -42,7 +42,7 @@ router.post('/insertUser', (req, res) => {
 });
 
 // route to get user login information --> get email & password
-router.get('/fetchUser', (req, res) => {
+router.get('/verifyUserLogin', (req, res) => {
   const loginEmail = req.query.email;
   const loginPassword = req.query.password;
   let query = `SELECT * FROM Users WHERE email='` + loginEmail + `';`;
@@ -71,23 +71,77 @@ router.get('/fetchUser', (req, res) => {
   });
 });
 
-// route to UPDATE user sysPref information
-router.put('/fetchSysPrefInfo', (req, res) => {
-  // 1. What do i put for 'req."something".password'
-
-  // const loginEmail = req.body.;
-  // const loginPassword = req.query.password;
-
-  // 2. Come up with an UPDATE Query to update the user table
-  // with new info
-
-  // let query =
-  //   `UPDATE * FROM Users
-  //   WHERE email='` + loginEmail + `' AND password='` + loginPassword + `';`;
-  //   console.log(query);
+// route to fetch all user Details
+router.get('/fetchUserDetails', (req, res) => {
+  const loginEmail = req.query.email;
+  let query = `SELECT * FROM Users WHERE email='` + loginEmail + `';`;
   database.query(query, (err, result) => {
-    if (err) console.log(err);
-    else res.send(result);
+    if (err) {
+      res.status(500).send('Cannot Fetch User Data');
+    } else {
+      if (result.length === 0) {
+        // user doesn't exist
+        res.status(404).send('Incorrect Credentials!');
+      } else {
+        res.status(200).send(result);
+      }
+    }
+  });
+});
+
+// route to UPDATE user sysPref information
+router.put('/updateSysPrefInfo', (req, res) => {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const email = req.body.email;
+  let tempQuery1 =
+    `UPDATE Users Set first_name='` +
+    first_name +
+    `', last_name='` +
+    last_name +
+    `', `;
+  let tempQuery2 =
+    req.body.street_address !== ''
+      ? `street_address='` + req.body.street_address + `', `
+      : `street_address=null, `;
+  let tempQuery3 =
+    req.body.city !== '' ? `city='` + req.body.city + `', ` : `city=null, `;
+  let tempQuery4 =
+    req.body.state !== '' ? `state='` + req.body.state + `', ` : `state=null, `;
+  let tempQuery5 =
+    req.body.zipcode !== ''
+      ? `zipcode='` + req.body.zipcode + `', `
+      : `zipcode=null, `;
+  // Encrypt Pwd - if changed
+  let tempQuery6 =
+    req.body.password !== ''
+      ? `password='` + bcrypt.hashSync(req.body.password, 10) + `', ` // change with new pwd
+      : ``; // password cannot be null
+  let tempQuery7 =
+    req.body.phone_number !== ''
+      ? `phone_number='` + req.body.phone_number + `' `
+      : `phone_number=null `;
+  let tempQuery8 = `WHERE email='` + email + `';`;
+
+  // Form the Update Query
+  // UPDATE Query that updates the user table with new info
+  let query =
+    tempQuery1 +
+    tempQuery2 +
+    tempQuery3 +
+    tempQuery4 +
+    tempQuery5 +
+    tempQuery6 +
+    tempQuery7 +
+    tempQuery8;
+  console.log(query);
+  database.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error Saving User Info');
+    } else {
+      res.status(200).send('User Details Saved Successfully!!');
+    }
   });
 });
 
