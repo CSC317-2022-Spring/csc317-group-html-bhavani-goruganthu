6ba5654159f0275d817f21844728afe369a8cbc9
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-09 01:17:12 -0700

    getUser & logout fetch routes on the navbar

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -61,49 +61,103 @@ function searchClick() {
   ).href = `./src/search.html?category=${selectedCategory}&keyword=${searchKeyword}`;
 }
 
-// code for changing login/signup button on the navbar to change to Logout for sysPref pagevar url =
-let isLoggedIn = localStorage.getItem('isLoggedIn');
-if (isLoggedIn === 'true') {
-  //remove Log In Button div from the navbar
-  document.getElementById(
-    'login-logout-btn-div'
-  ).innerHTML = `<a target="_parent" class="all-links-text-decoration">
-  <button class="all-nav-btns">
-      <span class="material-icons"  id="login-logout-icon">
-          logout
-      </span>
-  </button>
-  <br />
-  <h5 class="nav-link-name" id="login-logout-text"> Logout</h5>
-</a>`;
-  let settingsElement = `<div class="nav-links">
-    <a href="./src/sysPref.html" target="_parent" class="all-links-text-decoration">
-        <button class="all-nav-btns">
-            <span class="material-icons">
-                settings
-            </span>
-        </button>
-        <br />
-        <h5 class="nav-link-name"> Settings</h5>
-    </a>
-</div>`;
-  document.getElementById('nav-btns-div').innerHTML += settingsElement;
-}
-
+// adding login button click event to show login page
 document
   .getElementById('login-logout-btn-div')
-  .addEventListener('click', handleLogout);
-function handleLogout() {
+  .addEventListener('click', handleLogInClick);
+function handleLogInClick() {
   let loginLogoutIconText =
     document.getElementById('login-logout-icon').innerText;
   if (loginLogoutIconText === 'login') {
     window.parent.location.pathname = '/src/login.html';
-  } else if (loginLogoutIconText === 'logout') {
-    var userAnswer = window.confirm('You will be logged out. Are you sure?');
-    if (userAnswer) {
-      console.log('logged out');
-      localStorage.setItem('isLoggedIn', false);
-      window.location.reload();
+  }
+}
+
+// code for changing login/signup button on the navbar to change to Logout for sysPref pagevar url =
+// fetch login status from backend auth route
+fetch('http://localhost:4000/userAuth/getUser', {
+  // Adding method type
+  method: 'GET',
+  credentials: 'include',
+})
+  .then((response) => {
+    if (!response.ok) {
+      // get the text from the response object
+      const text = response.text();
+      throw new Error(text);
+    } else {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    updateNavIconsAfterLogin();
+  })
+  .catch((err) => {
+    // throw caught error as an alert
+    console.log(err);
+  });
+
+function updateNavIconsAfterLogin() {
+  //remove Log In Button div from the navbar
+  document.getElementById(
+    'login-logout-btn-div'
+  ).innerHTML = `<a target="_parent" class="all-links-text-decoration">
+<button class="all-nav-btns">
+    <span class="material-icons"  id="login-logout-icon">
+        logout
+    </span>
+</button>
+<br />
+<h5 class="nav-link-name" id="login-logout-text"> Logout</h5>
+</a>`;
+  let settingsElement = `<div class="nav-links">
+  <a href="./src/sysPref.html" target="_parent" class="all-links-text-decoration">
+      <button class="all-nav-btns">
+          <span class="material-icons">
+              settings
+          </span>
+      </button>
+      <br />
+      <h5 class="nav-link-name"> Settings</h5>
+  </a>
+</div>`;
+  document.getElementById('nav-btns-div').innerHTML += settingsElement;
+
+  document
+    .getElementById('login-logout-btn-div')
+    .addEventListener('click', handleLogout);
+  function handleLogout() {
+    let loginLogoutIconText =
+      document.getElementById('login-logout-icon').innerText;
+    if (loginLogoutIconText === 'login') {
+      window.parent.location.pathname = '/src/login.html';
+    } else if (loginLogoutIconText === 'logout') {
+      var userAnswer = window.confirm('You will be logged out. Are you sure?');
+      if (userAnswer) {
+        fetch('http://localhost:4000/userAuth/logout', {
+          // Adding method type
+          method: 'GET',
+          credentials: 'include',
+        })
+          .then((response) => {
+            if (!response.ok) {
+              // get the text from the response object
+              const text = response.text();
+              throw new Error(text);
+            } else {
+              return response.text();
+            }
+          })
+          .then((data) => {
+            console.log(data);
+            window.location.reload();
+          })
+          .catch((err) => {
+            // throw caught error as an alert
+            console.log(err);
+          });
+      }
     }
   }
 }
