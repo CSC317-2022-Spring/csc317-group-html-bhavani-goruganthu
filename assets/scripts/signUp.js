b23e18c36bbb2c7fd3d08434891887d3797096ca
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-08 17:08:29 -0700

    inserting user info to the db with error handling

--- assets/scripts/signUp.js
+++ assets/scripts/signUp.js
@@ -1,62 +1,58 @@
-// array of users
-// const user_table = JSON.parse(localStorage.getItem('users')) || [];
-fetch('http://localhost:4000/api/user/insertUser')
-
-document.getElementById('SignUp').addEventListener('click', localStore);
-
+// reset the form
 function resetChanges() {
   document.getElementById('signUpForm').reset();
 }
 
-function localStore(e) {
+// listen for form submit event
+document.getElementById('signUpForm').addEventListener('submit', insertUser);
+
+// work on insertUser API
+function insertUser(e) {
   e.preventDefault();
-
   // email & passwd
-  // let email = document.getElementById('email').value;
-  // let psswd1 = document.getElementById('psswd1').value;
-  // let psswd2 = document.getElementById('psswd2').value;
-
-  fetch('http://localhost:4000/api/user/insertUser')
-  .then((response) => {
-    console.log(data);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);    
-    // 1. IDK WHY IT DOES NOT WORK -> Data is not defined??
-
-    // if(data.length == 0){
-    //   // if user does not exist
-      
-    // }else if(data.length == 1){
-    //   // if user exists
-      
-    // }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  // if (psswd1 != psswd2) {
-  //   alert('Passwords Do Not Match.');
-  // } else {
-  //   // check if user has alread signed up.
-  //   if (
-  //     user_table.some((v) => {
-  //       return v.email == email;
-  //     })
-  //   ) {
-  //     alert('duplicate data');
-  //   } else {
-  //     // pushing data into array of users.
-  //     user_table.push({
-  //       email: email,
-  //       password: psswd1,
-  //     });
-
-  //     localStorage.setItem('users', JSON.stringify(user_table));
-
-  //     // send user to login page after signing up
-  //     window.location.pathname = '/src/login.html';
-  //   }
-  // }
+  let email = document.getElementById('email').value;
+  let psswd1 = document.getElementById('psswd1').value;
+  let psswd2 = document.getElementById('psswd2').value;
+  let fname = document.getElementById('fName').value;
+  let lname = document.getElementById('lName').value;
+  if (psswd1 !== psswd2) {
+    alert('Passwords Do Not Match!');
+  } else {
+    fetch('http://localhost:4000/api/users/insertUser', {
+      // Adding method type
+      method: 'POST',
+      // Adding body or contents to send
+      body: JSON.stringify({
+        email: email,
+        pwd: psswd1,
+        fname: fname,
+        lname: lname,
+      }),
+      // Adding headers to the request
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          // get the text from the response object
+          const text = await response.text();
+          throw new Error(text);
+        } else {
+          return response.text();
+        }
+      })
+      .then((msg) => {
+        // success alert
+        alert(msg);
+        // send user to login page after signing up
+        window.location.pathname = '/src/login.html';
+      })
+      .catch((err) => {
+        // throw the error as an alert
+        alert(err);
+        // reset the form
+        resetChanges();
+      });
+  }
 }
